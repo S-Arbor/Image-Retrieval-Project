bdd2d5e7ac797d2b5d070fc7f0c6467b09e75a2a
Sean Sutton <22981676@student.uwa.edu.au>
2022-01-26 11:38:23 +0800

    All 3 basic methods working!

--- 4. Clean/query.py
+++ 4. Clean/query.py
@@ -5,16 +5,14 @@ import numpy as np
 import diffusion
 
 def return_ranks(method, queries, gallery, **kwargs):
-    
+
     if method == "basic":
         ranks = np.array([basic_query(query, gallery, **kwargs) for query in queries])
         return(ranks)
-    
-    if method == "diffusion":
-    	ranks = diffusion.diffusion_ranks(queries, gallery, **kwargs)
-    	return(ranks)
-    
-    if method == "expanded query":
+    elif method == "diffusion":
+        ranks = diffusion.diffusion_ranks(queries, gallery, **kwargs)
+        return(ranks)
+    elif method == "expanded query":
         ranks = np.array([qe_query(query, gallery, **kwargs) for query in queries])
         return(ranks)
     
@@ -28,26 +26,21 @@ def basic_query(query, query_target, metric_function = sklearn.metrics.pairwise.
 
     return(index)
 
-def new_query(features, weights):
-    """Return a new formatted array containing a weighted combination of a previous set of features."""
-    return(pd.DataFrame([features.apply(np.average, weights = weights)]).values)
-
-
-def qe_query(query, query_target, metric_function=sklearn.metrics.pairwise.euclidean_distances, qe_type="qe baseline", n=5, alpha=1):
+def qe_query(query, query_target, metric_function=sklearn.metrics.pairwise.euclidean_distances, type="qe baseline", n=5, alpha=1):
     """Run a query with query expansion, supported methods:
        - "qe baseline" : described in Total Recall (2007), new result is based on alpha proportion of requerying (e.g. alpha = 1,
                          then results after the top 5 will be completely determined by the top five"""
     
     original_results = basic_query(query, query_target, metric_function)
 
-    if qe_type == "qe baseline":
+    if type == "qe baseline":
         # find top n results, combine top n into a new query, append results of new query to top n
-        top_n = query_target.loc[original_results[:n]]
-        second_query = new_query(top_n, weights = np.ones(n))
+        top_n = query_target[:n]
+        second_query = np.average(top_n, axis=0)
         
         if alpha != 1:
             combined_queries = np.vstack([query, second_query])
-            second_query = new_query(pd.DataFrame(combined_queries), weights = [1 - alpha, alpha])
+            second_query = np.average([query, second_query], axis = 0, weights = [1-alpha, alpha])
 
         new_results = basic_query(second_query, query_target, metric_function)
         pruned_new_results = new_results[np.logical_not(np.isin(new_results, top_n))]
